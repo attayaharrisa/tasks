08c888412f7b1bf5f5ff404874578f06213fa3e9
Anthony Attaya-Harris <anthony.attayaharris@maine.edu>
2022-10-11 14:30:50 -0400

    Fixed the add3 Function

--- src/functions.ts
+++ src/functions.ts
@@ -14,7 +14,17 @@ export function fahrenheitToCelius(temperature: number): number {
  * if the number is greater than zero.
  */
 export function add3(first: number, second: number, third: number): number {
-    return 0;
+    let temp: number = 0;
+    if (first > 0) {
+        temp += first;
+    }
+    if (second > 0) {
+        temp += second;
+    }
+    if (third > 0) {
+        temp += third;
+    }
+    return temp;
 }
 
 /**
