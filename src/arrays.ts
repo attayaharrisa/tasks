74e9798beb7e41b05e6061fcc0624a52aa3a3cf4
Anthony Attaya-Harris <anthony.attayaharris@maine.edu>
2022-10-22 11:34:13 -0400

    Fixed the tripleNumbers function

--- src/arrays.ts
+++ src/arrays.ts
@@ -19,7 +19,11 @@ export function bookEndList(numbers: number[]): number[] {
  * number has been tripled (multiplied by 3).
  */
 export function tripleNumbers(numbers: number[]): number[] {
-    return numbers;
+    let newNumbers = [];    
+    for (let i = 0; i < numbers.length; i++) {
+        newNumbers.push(numbers[i] * 3);    
+    }
+    return newNumbers;
 }
 
 /**
