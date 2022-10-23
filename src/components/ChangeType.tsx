de3acc2baa15ac272b58b1e8b66ed5b8a9a82bf9
Anthony Attaya-Harris <anthony.attayaharris@maine.edu>
2022-10-23 11:53:04 -0400

    Finished ChangeType.tsx

--- src/components/ChangeType.tsx
+++ src/components/ChangeType.tsx
@@ -1,7 +1,28 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
+import { scryRenderedComponentsWithType } from "react-dom/test-utils";
 import { QuestionType } from "../interfaces/question";
 
 export function ChangeType(): JSX.Element {
-    return <div>Change Type</div>;
+    const [questionType, setType] = useState<QuestionType>(
+        "short_answer_question"
+    );
+    function flipType(): void {
+        setType(
+            questionType === "multiple_choice_question"
+                ? "short_answer_question"
+                : "multiple_choice_question"
+        );
+    }
+    return (
+        <span>
+            <Button onClick={flipType}>Change Type</Button>
+            {questionType == "short_answer_question" && (
+                <div>{"Short Answer"}</div>
+            )}
+            {questionType == "multiple_choice_question" && (
+                <div>{"Multiple Choice"}</div>
+            )}
+        </span>
+    );
 }
