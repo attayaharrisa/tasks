59161ea281131a5d5f202fa78a862a5ef6a6b3b4
Anthony Attaya-Harris <anthony.attayaharris@maine.edu>
2022-12-16 09:55:36 -0500

    Fixed App.tsx issues

--- src/App.tsx
+++ src/App.tsx
@@ -26,57 +26,6 @@ function App(): JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-<<<<<<< HEAD
-                <h1>This is a header for HTML and CSS Task</h1>
-            </header>
-            <p>
-                <img src={computer} alt="This is where a picture should be" />
-                Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload. Anthony Attaya-Harris Hello World.
-            </p>
-            <ol>
-                <li>This is the first element</li>
-                <li>This is the seconds element</li>
-                <li>This is the third element</li>
-                <li>This is the fourth element</li>
-            </ol>
-            <Button onClick={() => console.log("Hello World!")}>
-                Log Hello World
-            </Button>
-            <Container>
-                <Row>
-                    <Col>
-                        First Column
-                        <div
-                            style={{
-                                width: 100,
-                                height: 100,
-                                backgroundColor: "red"
-                            }}
-                        >
-                            This is a red box
-                        </div>
-                    </Col>
-                    <Col>
-                        Second Column. With more stuff
-                        <div
-                            style={{
-                                width: 100,
-                                height: 100,
-                                backgroundColor: "red"
-                            }}
-                        >
-                            This is a red box
-                        </div>
-                    </Col>
-                </Row>
-            </Container>
-            <header>
-                <div>UM COS420 with React Hooks and TypeScript</div>
-            </header>
-            <hr></hr>
-            {<DoubleHalf></DoubleHalf>}
-=======
                 UM COS420 with React Hooks and TypeScript
             </header>
             <hr></hr>
@@ -94,7 +43,6 @@ function App(): JSX.Element {
             ></MultipleChoiceQuestion>
             <hr></hr>
             {/* <DoubleHalf></DoubleHalf> */}
->>>>>>> upstream/task-forms
             <hr></hr>
             <ChooseTeam></ChooseTeam>
             <hr></hr>
